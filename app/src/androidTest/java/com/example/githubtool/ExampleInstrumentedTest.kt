78f84e8815e855d50a7269cdd830ee13531704af
Tuan Minh Vu <tuanminh.vu@airpay.com>
2021-05-18 19:45:47 +0700

    update

--- app/src/androidTest/java/com/example/githubtool/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/example/githubtool/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.example.ourmr", appContext.packageName)
+        assertEquals("com.example.githubtool", appContext.packageName)
     }
 }
